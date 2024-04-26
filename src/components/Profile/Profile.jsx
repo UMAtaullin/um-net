73781781bda1b44f94ca5f6c185812cc55676cf2
Ural Ataullin <ataullinural@gmail.com>
2024-04-26 16:13:57 +0500

    use dispatch

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -9,8 +9,7 @@ const Profile = (props) => {
       <MyPosts
         postsData={props.profilePage.postsData}
         newPostText={props.profilePage.newPostText}
-        updateNewPostText={props.updateNewPostText}
-        addPost={props.addPost}
+        dispatch={props.dispatch}
       />
     </div>
   )
--- src/components/redux/state.js
+++ src/components/redux/state.js
@@ -5,7 +5,7 @@ let store = {
         { message: 'Hi, it is me', likes: 5 },
         { message: 'Where have you been', likes: 14 },
       ],
-      newPostText: 'new text'
+      newPostText: 'new text',
     },
     messengerPage: {
       dialogsData: [
@@ -23,32 +23,34 @@ let store = {
         { message: 'You can also take the bus.' },
       ],
     },
-    sidebar: {}
-  },
-  getState() {
-    debugger;
-    return this._state
+    sidebar: {},
   },
   _callSubscriber() {
     console.log('State changed')
   },
-  addPost() {
-    debugger;
-    let newPost = {
-      message: this._state.profilePage.newPostText,
-      likes: 0
-    };
-    this._state.profilePage.postsData.push(newPost);
-    this._state.profilePage.newPostText = '';
-    this._callSubscriber(this._state);
-  },
-  updateNewPostText(newText) {
-    this._state.profilePage.newPostText = newText;
-    this._callSubscriber(this._state)
+
+  getState() {
+    debugger
+    return this._state
   },
   subscribe(observer) {
     this._callSubscriber = observer
   },
+  // Методы которые меняют на state.
+  dispatch(action) { // {type: 'ADD-POST}
+    if (action.type === 'ADD-POST') {
+      let newPost = {
+        message: this._state.profilePage.newPostText,
+        likes: 0,
+      }
+      this._state.profilePage.postsData.push(newPost)
+      this._state.profilePage.newPostText = ''
+      this._callSubscriber(this._state)
+    } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+        this._state.profilePage.newPostText = action.newText
+        this._callSubscriber(this._state)
+    }
+  }
 }
 
 export default store;
